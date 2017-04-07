0d445ac03629f7b95bcb14431b2b5e35b4907f65
JK Gunnink <jgunnink@gmail.com>
2017-04-07 09:39:09 +0800

    remove unused callback code

--- web/src/store/loaders/index.js
+++ web/src/store/loaders/index.js
@@ -2,15 +2,6 @@ import request from "../../util/agent"
 import { receiveLogin } from "../actions/authentication"
 import { push } from "redux-little-router"
 
-let loaded = {assets:false, pois: false}
-const callback = (name, cb) => () => {
-	loaded[name] = true
-	for (let key of Object.keys(loaded)) {
-		if (key === false) return
-	}
-	cb()
-}
-
 const loadState = (dispatch, cb) => {
 	request.get("/check_login").then((res) => {
 		dispatch(receiveLogin(res.data))
