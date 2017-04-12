5781f6ea3c04e7e0f795964f6844093d8a52af0b
JK Gunnink <jgunnink@gmail.com>
2017-04-12 16:11:59 +0800

    prepopulates edit user fields

--- web/src/containers/MyAccount/index.jsx
+++ web/src/containers/MyAccount/index.jsx
@@ -1,18 +1,29 @@
 import React from "react"
-import MyAccount from "../../components/MyAccount"
+import AccountDetailsForm from "../../components/MyAccount"
 import { connect } from "react-redux"
 import { accountUpdate } from "../../store/actions/accountUpdate"
 
 const UserAccount = (props) => {
-  return(
-	  <MyAccount accountUpdate={props.accountUpdate} errorCode={props.errorCode} />
-  )
+	const user = props.user
+	return(
+		<AccountDetailsForm
+			accountUpdate={props.accountUpdate}
+			errorCode={props.errorCode} 
+			initialValues={{
+				email: user.get("email"),
+				first: user.get("firstName"),
+				last: user.get("lastName")
+			}}
+		/>
+	)
 }
 
 const mapStateToProps = (state, ownProps) => {
 	const errorCode = state.railway.accountUpdate.get("status")
+	const user = state.railway.authentication.get("user")
 	return {
-		errorCode
+		errorCode,
+		user: user
 	}
 }
 
